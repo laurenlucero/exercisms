9612e65bcec0c4d5571bc58ab8bc816a877f63af
Lauren Lucero <54958511+laurenlucero@users.noreply.github.com>
2021-01-22 11:55:05 -0500

    Update two-fer.js

--- two-fer.js
+++ two-fer.js
@@ -1,4 +1,4 @@
-// if/else
+/* if/else */
 
 // export const twoFer = (name) => {
 //   if (!name) {
@@ -8,6 +8,10 @@
 //   }
 // };
 
-// ternary
+/* ternary */
 
-export const twoFer = (name) => !name ? 'One for you, one for me.' : `One for ${name}, one for me.`
+// export const twoFer = (name) => !name ? 'One for you, one for me.' : `One for ${name}, one for me.`
+
+/* default value for parameter */
+
+export const twoFer = (name = "you") => `One for ${name}, one for me.`;
