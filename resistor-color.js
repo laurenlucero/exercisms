82bbd4b34c2c728affca813e5b9e554bad8b1d2b
Lauren Lucero <54958511+laurenlucero@users.noreply.github.com>
2021-01-24 16:31:41 -0500

    Add files via upload

---
+++ resistor-color.js
@@ -0,0 +1,18 @@
+// read the first bands color from an array
+// return it's numeric value
+// indexOf() method returns the first index at which a given element can be found in the array or -1 if element is not found
+
+export const colorCode = (color) => COLORS.indexOf(color);
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
