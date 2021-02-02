3c17ec5930b38072774815a397065a3c6950ba89
Lauren Lucero <54958511+laurenlucero@users.noreply.github.com>
2021-02-02 11:46:37 -0500

    Update space-age.js

--- space-age.js
+++ space-age.js
@@ -8,20 +8,24 @@
 // Uranus: orbital period 84.016846 Earth years
 // Neptune: orbital period 164.79132 Earth years
 
+const earthYearInSecs = 31557600;
+
+// object holds orbital period value of each planet in Earth years
+const spaceTime = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
 // pass in planet name and seconds as arguments
 export const age = (planet, seconds) => {
-  // object holds orbital period value of each planet in Earth years
-  const spaceTime = {
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    earth: 1,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132,
-  };
+  // Number() converts to Number type
   // divide seconds argument by Earth orbit seconds multiplied by planet spaceTime value
   // toFixed() method formats a number using fixed-point notation
-  return (seconds / (31557600 * spaceTime[planet])).toFixed(2) * 1;
+  return Number((seconds / (earthYearInSecs * spaceTime[planet])).toFixed(2));
 };
